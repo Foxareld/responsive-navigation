6300b5efadc840475218bedb26c9f6eb5bdfa594
Alfredo <al.ramos777@gmail.com>
2013-05-05 18:54:01 -0400

    added document.ready and window.resize functions

--- response_nav.js
+++ response_nav.js
@@ -1,12 +1,23 @@
-//Responsive Nav Menu
-var lefty,righty;
-var navWidth = $(".resp_nav").width(); //get width of nav container
-var liAmt = $(".resp_nav li").length; //get number of list items
-$(".resp_nav li").each(function(index){
-	lefty = parseInt($(this).css('padding-left'));
-	righty = parseInt($(this).css('padding-right'));
+function responseNav(){
+	//Responsive Nav Menu
+	var lefty,righty;
+	var navWidth = $(".resp_nav").width(); //get width of nav container
+	var liAmt = $(".resp_nav li").length; //get number of list items
+	$(".resp_nav li").each(function(index){
+		lefty = parseInt($(this).css('padding-left'));
+		righty = parseInt($(this).css('padding-right'));
+	});
+	var liPad = lefty+righty; //account for padding, must parseInt because string is returned
+	var newWidth = (navWidth/liAmt)- (liPad+2); //calculate new width for list items, subtract 1 as buffer
+	$(".resp_nav li a").css('display','inline-block'); //must change display so width change will work
+	$(".resp_nav li a").width(newWidth);	
+		
+}
+
+$(document).ready(function(){
+	responseNav();
+	
+	$(window).resize(function(){
+		responseNav(true);
+	})
 });
-var liPad = lefty+righty; //account for padding, must parseInt because string is returned
-var newWidth = (navWidth/liAmt)- (liPad+2); //calculate new width for list items, subtract 1 as buffer
-$(".resp_nav li a").css('display','inline-block'); //must change display so width change will work
-$(".resp_nav li a").width(newWidth);
